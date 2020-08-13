2b8d413effa6bd68d1343aae6c651c72f922fb46
rochmadnurdin <69510468+rochmadnurdin@users.noreply.github.com>
2020-08-13 16:34:38 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 
-# mkdir output
-# mkdir output/docs
-# mkdir output/docs/Silo-V2
-# mkdir output/docs/images
-
 mkdir -p output/docs/{Silo-V2,images}
 
-
 cp -R docs output/
-
-
-
-
-
-
 
 
 
