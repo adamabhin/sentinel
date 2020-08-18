557da4b53de4883cfc5d7c476cc3396809ed74da
tegarimansyah <egar.imansyah@gmail.com>
2020-08-18 14:42:15 +0700

    add system name in params

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,18 @@ DEST_GITHUB_USERNAME="$2"
 DEST_GITHUB_REPO="$3"
 USER_EMAIL="$4"
 DEST_BRANCH="$5"
+SYSTEM_NAME="$6"
 
 CLONE_DIR=$(mktemp -d)
 
 # Setup git
-echo "You are $USER_EMAIL $DEST_GITHUB_USERNAME"
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$DEST_GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$DEST_GITHUB_USERNAME/$DEST_GITHUB_REPO.git" "$CLONE_DIR"
 
-echo 'Copying from '"$SRC_DIR"'/*' "to $CLONE_DIR/$REPOSITORY_NAME"
-mkdir -p "$CLONE_DIR/$REPOSITORY_NAME"
-cp -R "$SRC_DIR"/* "$CLONE_DIR/$REPOSITORY_NAME" || (rm -Rf "$CLONE_DIR" && exit 1)
+echo 'Copying from '"$SRC_DIR"'/*' "to $CLONE_DIR/$SYSTEM_NAME"
+mkdir -p "$CLONE_DIR/$SYSTEM_NAME"
+cp -R "$SRC_DIR"/* "$CLONE_DIR/$SYSTEM_NAME" || (rm -Rf "$CLONE_DIR" && exit 1)
 
 cd "$CLONE_DIR"
 git add .
