5ea603669ebbf90d4a239169db5d881f147fcb20
Tegar Imansyah <egar.imansyah@gmail.com>
2020-08-18 14:29:57 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ CLONE_DIR=$(mktemp -d)
 # Setup git
 echo "You are $USER_EMAIL $GITHUB_USERNAME"
 git config user.email "$USER_EMAIL"
-git config user.name "$GITHUB_USERNAME"
+git config user.name "$DEST_GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$DEST_GITHUB_USERNAME/$DEST_GITHUB_REPO.git" "$CLONE_DIR"
 
 echo 'Copying from '"$SRC_DIR"'/*' "to $CLONE_DIR/$REPOSITORY_NAME"
