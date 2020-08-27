49e06edc47ca5244352f8e7348c1dd7dab8de120
rochmadnurdin <dev@alterra.id>
2020-08-27 14:39:23 +0700

    use run instead of dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 
 echo "Start"
-SRC_DIR="$1"
-DEST_GITHUB_USERNAME="$2"
-DEST_GITHUB_REPO="$3"
-USER_EMAIL="$4"
-DEST_BRANCH="$5"
-INITIATIVE_NAME="$6"
-SYSTEM_NAME="$7"
+# SRC_DIR="$1"
+# DEST_GITHUB_USERNAME="$2"
+# DEST_GITHUB_REPO="$3"
+# USER_EMAIL="$4"
+# DEST_BRANCH="$5"
+# INITIATIVE_NAME="$6"
+# SYSTEM_NAME="$7"
 
 CLONE_DIR=$(mktemp -d)
 
@@ -21,15 +21,20 @@ mkdir -p "$CLONE_DIR/$INITIATIVE_NAME/$SYSTEM_NAME"
 cp -R "$SRC_DIR"/* "$CLONE_DIR/$INITIATIVE_NAME/$SYSTEM_NAME" || (rm -Rf "$CLONE_DIR" && exit 1)
 cp readme.adoc "$CLONE_DIR/$INITIATIVE_NAME/$SYSTEM_NAME.adoc" || (rm -Rf "$CLONE_DIR" && exit 1)
 
+echo "Push to $GITHUB_REPOSITORY in branch $INITIATIVE_NAME"
 cd "$CLONE_DIR"
 git add .
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 git push origin $DEST_BRANCH
-curl --location --request POST 'https://api.github.com/repos/rochmadnurdin/knowledgebase/pulls?access_token=$API_TOKEN_GITHUB' \
+
+echo "Create Pull Request"
+curl --location --request POST "https://api.github.com/repos/$DEST_GITHUB_USERNAME/$DEST_GITHUB_REPO/pulls?access_token=$API_TOKEN_GITHUB" \
 --header 'Accept: application/vnd.github.v3+json' \
 --header 'Content-Type: application/json' \
---data-raw '{
-    "title": "Message Title",
-    "head": "$DEST_GITHUB_REPO",
-    "base": "BPA"
-}'
+--data-raw "{
+    \"title\": \"$SYSTEM_NAME at $(date)\",
+    \"head\": \"$INITIATIVE_NAME\",
+    \"base\": \"master\"
+}"
+
+echo "Done, thank you"
