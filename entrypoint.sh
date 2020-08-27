577059ea7f8361af9eff50c07499c3669c74bec8
rochmadnurdin <dev@alterra.id>
2020-08-27 14:51:34 +0700

    change pull request head branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,26 +15,27 @@ CLONE_DIR=$(mktemp -d)
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$DEST_GITHUB_USERNAME"
 git clone --single-branch -b $DEST_BRANCH "https://$API_TOKEN_GITHUB@github.com/$DEST_GITHUB_USERNAME/$DEST_GITHUB_REPO.git" "$CLONE_DIR"
+git checkout -b "$SYSTEM_NAME"
 
 echo 'Copying from '"$SRC_DIR"'/*' "to $CLONE_DIR/$INITIATIVE_NAME/$SYSTEM_NAME"
 mkdir -p "$CLONE_DIR/$INITIATIVE_NAME/$SYSTEM_NAME"
 cp -R "$SRC_DIR"/* "$CLONE_DIR/$INITIATIVE_NAME/$SYSTEM_NAME" || (rm -Rf "$CLONE_DIR" && exit 1)
 cp readme.adoc "$CLONE_DIR/$INITIATIVE_NAME/$SYSTEM_NAME.adoc" || (rm -Rf "$CLONE_DIR" && exit 1)
 
-echo "Push to $GITHUB_REPOSITORY in branch $INITIATIVE_NAME"
+echo "Push to $DEST_GITHUB_USERNAME/$DEST_GITHUB_REPO in branch $INITIATIVE_NAME"
 cd "$CLONE_DIR"
 git add .
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-git push origin $DEST_BRANCH
+git push origin "$SYSTEM_NAME"  || (echo "push to $DEST_GITHUB_USERNAME/$DEST_GITHUB_REPO failed" && exit 1)
 
 echo "Create Pull Request"
-curl --location --request POST "https://api.github.com/repos/$DEST_GITHUB_USERNAME/$DEST_GITHUB_REPO/pulls?access_token=$API_TOKEN_GITHUB" \
+curl --location -s --request POST "https://api.github.com/repos/$DEST_GITHUB_USERNAME/$DEST_GITHUB_REPO/pulls?access_token=$API_TOKEN_GITHUB" \
 --header 'Accept: application/vnd.github.v3+json' \
 --header 'Content-Type: application/json' \
 --data-raw "{
     \"title\": \"$SYSTEM_NAME at $(date)\",
-    \"head\": \"$INITIATIVE_NAME\",
-    \"base\": \"master\"
-}"
+    \"head\": \"$SYSTEM_NAME\",
+    \"base\": \"$DEST_BRANCH\"
+}" || (echo "Creating pull request failed" && exit 1)
 
 echo "Done, thank you"
